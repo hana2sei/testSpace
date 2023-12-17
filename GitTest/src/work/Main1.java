3721a70f5e32ed71a85542b7c782e4c165c909bb
EZPAD\hana2 <hana2sei@gmail.com>
2023-12-17 14:57:03 +0900

    キーボード入力対応追加

--- GitTest/src/work/Main1.java
+++ GitTest/src/work/Main1.java
@@ -5,6 +5,13 @@ public class Main1 {
 	public static void main(String[] args) {
 		System.out.println("こんにちは");
 		System.out.println("こんばんは");
+		
+		int input = new java.util.Scanner(System.in).nextInt();
+		if (input == 1) {
+			System.out.println("1を入力しましたね");
+		} else {
+			System.out.println("1以外を入力しましたね");
+		}
 	}
 
 }
