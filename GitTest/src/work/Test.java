ef73a555f2e65a04dedb085062326e12c135cb7a
EZPAD\hana2 <hana2sei@gmail.com>
2023-12-19 21:57:48 +0900

    Testクラスを追加

---
+++ GitTest/src/work/Test.java
@@ -0,0 +1,20 @@
+package work;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int key = 100;
+		switch (key) {
+			case 1, 2 -> {
+				System.out.println("1st");
+			}
+			case 3 -> {
+				System.out.println("2nd");
+			}
+			default -> {
+				System.out.println("3rd");
+			}
+		}
+	}
+
+}
