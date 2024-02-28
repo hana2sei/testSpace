4a83cb4c12d80b73d7f22bf55bb5de34450e0c3d
EZPAD\hana2 <hana2sei@gmail.com>
2024-02-28 22:08:31 +0900

    firstCommit

---
+++ SpringShohinKanri_Simple/src/main/java/com/example/SpringShohinKanriSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShohinKanriSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShohinKanriSimpleApplication.class, args);
+	}
+
+}
